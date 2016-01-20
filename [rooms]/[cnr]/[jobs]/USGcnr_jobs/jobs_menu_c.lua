7f3de6de0643249d5ff35d91a428d49c3a7e7eb9
Soap <ralph_367@hotmail.com>
2016-01-20 21:36:41 +0200

    no message

--- [rooms]/[cnr]/[jobs]/USGcnr_jobs/jobs_menu_c.lua
+++ [rooms]/[cnr]/[jobs]/USGcnr_jobs/jobs_menu_c.lua
@@ -1,12 +1,13 @@
 jobMenuGUI = {}
 
+
 function createJobMenu()
     local screenW, screenH = guiGetScreenSize()
-    jobMenuGUI.window = guiCreateWindow((screenW - 219) / 2, (screenH - 199) / 2, 219, 199, "Employment", false)
+    jobMenuGUI.window = guiCreateWindow((screenW - 264) / 2, (screenH - 259) / 2, 264, 259, "Employment", false)
     guiWindowSetSizable(jobMenuGUI.window, false)
     jobMenuGUI.close  = guiCreateButton(66, 183, 104, 37, "Close", false, jobMenuGUI.window)
     jobMenuGUI.quit = guiCreateButton(65, 143, 105, 35, "Quit job", false,jobMenuGUI.window)
-    jobMenuGUI.info = guiCreateLabel(42, 44, 84, 42, "Currently: "..getPlayerOccupation() or "unemployed", false, jobMenuGUI.window)
+    jobMenuGUI.info = guiCreateLabel(42, 44, 190, 42, "Currently: "..getPlayerOccupation() or "unemployed", false, jobMenuGUI.window)
     addEventHandler("onClientGUIClick", jobMenuGUI.quit, onQuitJob, false)
     addEventHandler("onClientGUIClick", jobMenuGUI.close, closeJobMenu, false)
 end
@@ -15,16 +16,16 @@ function openJobMenu()
     if(not isElement(jobMenuGUI.window)) then
         createJobMenu()
         showCursor("menu", true)
-    elseif(not exports.USGGUI:getVisible(jobMenuGUI.window)) then
+    elseif(not guiGetVisible(jobMenuGUI.window)) then
         showCursor("menu", true)
-        exports.USGGUI:setVisible(jobMenuGUI.window, true)
+        guiGetVisible(jobMenuGUI.window, true)
     end
-    exports.USGGUI:setText(jobMenuGUI.info, "Currently: "..getPlayerOccupation() or "unemployed")
+    guiSetText(jobMenuGUI.info, "Currently: "..getPlayerOccupation() or "unemployed")
 end
 
 function closeJobMenu()
     if(isElement(jobMenuGUI.window) and guiGetVisible(jobMenuGUI.window)) then
-        exports.USGGUI:setVisible(jobMenuGUI.window, false)
+        guiGetVisible(jobMenuGUI.window, false)
         showCursor("menu", false)
     end
 end
