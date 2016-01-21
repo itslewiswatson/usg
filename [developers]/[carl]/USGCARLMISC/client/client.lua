f919cf4bfecc0a4c80e58f3e332c0d173faeaf8c
Carl Rizk <carlrizk88@gmail.com>
2016-01-21 17:45:13 +0200

    no message

--- [developers]/[carl]/USGCARLMISC/client/client.lua
+++ [developers]/[carl]/USGCARLMISC/client/client.lua
@@ -1,7 +1,9 @@
 function render()
 	
+	local playerRoom = exports.USGrooms:getPlayerRoom(localPlayer)
 	local x,y,z = getElementPosition(localPlayer)
 	local Mult = 1
+	
 		if(getPedOccupiedVehicle (localPlayer))then
 			local vehicle = getPedOccupiedVehicle (localPlayer)
 			x,y,z = getElementPosition(vehicle)
@@ -13,14 +15,28 @@ function render()
 			
 		end
 		
-	dxDrawMaterialLine3D ( x + apps.account.position.x * Mult,		y + apps.account.position.y * Mult,	z + apps.account.position.z * Mult + apps.account.size.default * Mult,		x + apps.account.position.x * Mult,	y + apps.account.position.y * Mult,	z + apps.account.position.z * Mult,	apps.account.icon,	apps.account.size.default * Mult	)
-	dxDrawMaterialLine3D ( x + apps.call.position.x * Mult,		y + apps.call.position.y * Mult,		z + apps.call.position.z * Mult + apps.call.size.default * Mult,			x + apps.call.position.x * Mult,		y + apps.call.position.y * Mult,		z + apps.call.position.z * Mult,		apps.call.icon,		apps.call.size.default * Mult		)
-	dxDrawMaterialLine3D ( x + apps.gps.position.x * Mult,			y + apps.gps.position.y * Mult,		z + apps.gps.position.z * Mult + apps.gps.size.default * Mult,				x + apps.gps.position.x * Mult,	 	y + apps.gps.position.y * Mult,		z + apps.gps.position.z * Mult,		apps.gps.icon,		apps.gps.size.default * Mult		)
-	dxDrawMaterialLine3D ( x + apps.music.position.x * Mult,		y + apps.music.position.y * Mult,		z + apps.music.position.z * Mult + apps.music.size.default * Mult,			x + apps.music.position.x * Mult, 		y + apps.music.position.y * Mult,		z + apps.music.position.z * Mult,		apps.music.icon,	apps.music.size.default * Mult		)
-	dxDrawMaterialLine3D ( x + apps.weapons.position.x * Mult,		y + apps.weapons.position.y * Mult,	z + apps.weapons.position.z * Mult + apps.weapons.size.default * Mult,		x + apps.weapons.position.x * Mult,	y + apps.weapons.position.y * Mult,	z + apps.weapons.position.z * Mult,	apps.weapons.icon,	apps.weapons.size.default * Mult	)
-	dxDrawMaterialLine3D ( x + apps.messages.position.x * Mult,	y + apps.messages.position.y * Mult,	z + apps.messages.position.z * Mult + apps.messages.size.default * Mult,	x + apps.messages.position.x * Mult,	y + apps.messages.position.y * Mult,	z + apps.messages.position.z * Mult,	apps.messages.icon,	apps.messages.size.default * Mult	)
-	dxDrawMaterialLine3D ( x + apps.money.position.x * Mult,		y + apps.money.position.y * Mult,		z + apps.money.position.z * Mult + apps.money.size.default * Mult,			x + apps.money.position.x * Mult,		y + apps.money.position.y * Mult,		z + apps.money.position.z * Mult,		apps.money.icon,	apps.money.size.default * Mult		)
-
+	if(playerRoom == "cnr" or apps.account.allRooms)then
+		dxDrawMaterialLine3D ( x + apps.account.position.x * Mult,		y + apps.account.position.y * Mult,	z + apps.account.position.z * Mult + apps.account.size.default * Mult,		x + apps.account.position.x * Mult,	y + apps.account.position.y * Mult,	z + apps.account.position.z * Mult,	apps.account.icon,	apps.account.size.default * Mult	)
+	end
+	if(playerRoom == "cnr" or apps.call.allRooms)then
+		dxDrawMaterialLine3D ( x + apps.call.position.x * Mult,		y + apps.call.position.y * Mult,		z + apps.call.position.z * Mult + apps.call.size.default * Mult,			x + apps.call.position.x * Mult,		y + apps.call.position.y * Mult,		z + apps.call.position.z * Mult,		apps.call.icon,		apps.call.size.default * Mult		)
+	end
+	if(playerRoom == "cnr" or apps.gps.allRooms)then
+		dxDrawMaterialLine3D ( x + apps.gps.position.x * Mult,			y + apps.gps.position.y * Mult,		z + apps.gps.position.z * Mult + apps.gps.size.default * Mult,				x + apps.gps.position.x * Mult,	 	y + apps.gps.position.y * Mult,		z + apps.gps.position.z * Mult,		apps.gps.icon,		apps.gps.size.default * Mult		)
+	end
+	if(playerRoom == "cnr" or apps.music.allRooms)then
+		dxDrawMaterialLine3D ( x + apps.music.position.x * Mult,		y + apps.music.position.y * Mult,		z + apps.music.position.z * Mult + apps.music.size.default * Mult,			x + apps.music.position.x * Mult, 		y + apps.music.position.y * Mult,		z + apps.music.position.z * Mult,		apps.music.icon,	apps.music.size.default * Mult		)
+	end
+	if(playerRoom == "cnr" or apps.weapons.allRooms)then
+		dxDrawMaterialLine3D ( x + apps.weapons.position.x * Mult,		y + apps.weapons.position.y * Mult,	z + apps.weapons.position.z * Mult + apps.weapons.size.default * Mult,		x + apps.weapons.position.x * Mult,	y + apps.weapons.position.y * Mult,	z + apps.weapons.position.z * Mult,	apps.weapons.icon,	apps.weapons.size.default * Mult	)
+	end
+	if(playerRoom == "cnr" or apps.messages.allRooms)then
+		dxDrawMaterialLine3D ( x + apps.messages.position.x * Mult,	y + apps.messages.position.y * Mult,	z + apps.messages.position.z * Mult + apps.messages.size.default * Mult,	x + apps.messages.position.x * Mult,	y + apps.messages.position.y * Mult,	z + apps.messages.position.z * Mult,	apps.messages.icon,	apps.messages.size.default * Mult	)
+	end
+	if(playerRoom == "cnr" or apps.money.allRooms)then
+		dxDrawMaterialLine3D ( x + apps.money.position.x * Mult,		y + apps.money.position.y * Mult,		z + apps.money.position.z * Mult + apps.money.size.default * Mult,			x + apps.money.position.x * Mult,		y + apps.money.position.y * Mult,		z + apps.money.position.z * Mult,		apps.money.icon,	apps.money.size.default * Mult		)
+	end	
+		
 end
 
 addEventHandler("onClientRender", root,render)
