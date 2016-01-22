14bd4b73615bf8ef44b4f2d762f93f0012cdd8b1
Soap <ralph_367@hotmail.com>
2016-01-22 18:40:11 +0200

    no message

--- USGdevelopment/dev_c.lua
+++ USGdevelopment/dev_c.lua
@@ -2,33 +2,38 @@ local startX, startY
 local screenWidth, screenHeight = guiGetScreenSize()
 
 function measure1()
-	startX,startY = getCursorPosition()
-	outputConsole(string.format("%0.4f, %0.4f, %0.0f, %0.0f", startX, startY, startX*screenWidth, startY*screenHeight))
+    startX,startY = getCursorPosition()
+    outputConsole(string.format("%0.4f, %0.4f, %0.0f, %0.0f", startX, startY, startX*screenWidth, startY*screenHeight))
 end
 
 function measure2()
-	local endX, endY = getCursorPosition()
-	outputConsole(string.format("%0.4f, %0.4f, %0.0f, %0.0f", endX, endY, endX*screenWidth, endY*screenHeight))
-	local difX, difY = math.abs(endX-startX), math.abs(endY-startY)
-	outputConsole(string.format("Difference: %0.4f, %0.4f, %0.0f, %0.0f", difX, difY, difX*screenWidth, difY*screenHeight))
+    local endX, endY = getCursorPosition()
+    outputConsole(string.format("%0.4f, %0.4f, %0.0f, %0.0f", endX, endY, endX*screenWidth, endY*screenHeight))
+    local difX, difY = math.abs(endX-startX), math.abs(endY-startY)
+    outputConsole(string.format("Difference: %0.4f, %0.4f, %0.0f, %0.0f", difX, difY, difX*screenWidth, difY*screenHeight))
 end
 addCommandHandler("m1", measure1)
 addCommandHandler("m2", measure2)
 
 --[[addEventHandler("onClientDebugMessage", root,
-	function (msg,lvl,file, line)
-		triggerServerEvent("USGdevelopment.onClientDebugMessage", localPlayer, "["..(lvl or -1).."]"..(file or "")..":"..(line or "").." "..msg)
-	end
+    function (msg,lvl,file, line)
+        triggerServerEvent("USGdevelopment.onClientDebugMessage", localPlayer, "["..(lvl or -1).."]"..(file or "")..":"..(line or "").." "..msg)
+    end
 )
 -]]
 
 function convertNumber ( number )  
-	local formatted = number  
-	while true do      
-		formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')    
-		if ( k==0 ) then      
-			break   
-		end  
-	end  
-	return formatted
+    local formatted = number  
+    while true do      
+        formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')    
+        if ( k==0 ) then      
+            break   
+        end  
+    end  
+    return formatted
 end
+
+function matrix()
+getElementMatrix(localPlayer)
+end 
+addCommandHandler("matrix",matrix)
