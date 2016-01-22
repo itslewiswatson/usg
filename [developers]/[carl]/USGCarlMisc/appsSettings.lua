cd51d00ccc7e592f91d1184e0d468fe1dc79c28b
Carl Rizk <carlrizk88@gmail.com>
2016-01-22 19:15:29 +0200

    no message

---
+++ [developers]/[carl]/USGCarlMisc/appsSettings.lua
@@ -0,0 +1,124 @@
+apps = {}
+	apps.account = {}
+		apps.account.icon = dxCreateTexture("images/account_icon.png")
+		apps.account.name = "Account App"
+		apps.account.position = {}
+			apps.account.position.x = -1.5
+			apps.account.position.y = 1.25
+			apps.account.position.z = 0.5
+			
+		apps.account.size = {}
+			apps.account.size.default = 0.5
+			apps.account.size.selected = 0.75
+			
+		apps.account.allRooms = true
+		
+		apps.account.event = "UserPanel.App.AccountApp"
+	
+	apps.call = {}
+		apps.call.icon = dxCreateTexture("images/call_icon.png")
+		apps.call.name = "Call App"
+		apps.call.position = {}
+			apps.call.position.x = 0
+			apps.call.position.y = 1.5
+			apps.call.position.z = 0.5
+			
+		apps.call.size = {}
+			apps.call.size.default = 0.5
+			apps.call.size.selected = 0.75
+			
+		apps.call.allRooms = false
+		
+		apps.call.event = "UserPanel.App.CallApp"
+		
+	apps.gps = {}
+		apps.gps.icon = dxCreateTexture("images/gps_icon.png")
+		apps.gps.name = "GPS App"
+		apps.gps.position = {}
+			apps.gps.position.x = 1.5
+			apps.gps.position.y = 1.25
+			apps.gps.position.z = 0.5
+			
+		apps.gps.size = {}
+			apps.gps.size.default = 0.5
+			apps.gps.size.selected = 0.75
+			
+		apps.gps.allRooms = false
+		
+		apps.gps.event = "UserPanel.App.GPSApp"
+		
+	apps.messages = {}
+		apps.messages.icon = dxCreateTexture("images/messages_icon.png")
+		apps.messages.name = "Message App"
+		apps.messages.position = {}
+			apps.messages.position.x = -1.5
+			apps.messages.position.y = -1.25
+			apps.messages.position.z = 0.5
+			
+		apps.messages.size = {}
+			apps.messages.size.default = 0.5
+			apps.messages.size.selected = 0.75
+		
+		apps.messages.allRooms = true
+		
+		apps.messages.event = "UserPanel.App.MessageApp"
+		
+	apps.money = {}
+		apps.money.icon = dxCreateTexture("images/money_icon.png")
+		apps.money.name = "Money App"
+		apps.money.position = {}
+			apps.money.position.x = 1.75
+			apps.money.position.y = 0
+			apps.money.position.z = 0.5
+			
+		apps.money.size = {}
+			apps.money.size.default = 0.5
+			apps.money.size.selected = 0.75
+			
+		apps.money.allRooms = false
+		
+		apps.money.event = "UserPanel.App.MoneyApp"
+		
+	apps.music = {}
+		apps.music.icon = dxCreateTexture("images/music_icon.png")
+		apps.music.name = "Music App"
+		apps.music.position = {}
+			apps.music.position.x = 1.5
+			apps.music.position.y = -1.25
+			apps.music.position.z = 0.5
+			
+		apps.music.size = {}
+			apps.music.size.default = 0.5
+			apps.music.size.selected = 0.75
+			
+		apps.music.allRooms = true
+		
+		apps.music.event = "UserPanel.App.MusicApp"
+
+	apps.weapons = {}
+		apps.weapons.icon = dxCreateTexture("images/weapons_icon.png")
+		apps.weapons.name = "Weapon App"
+		apps.weapons.position = {}
+			apps.weapons.position.x = 0
+			apps.weapons.position.y = -1.25
+			apps.weapons.position.z = 0.5
+			
+		apps.weapons.size = {}
+			apps.weapons.size.default = 0.5
+			apps.weapons.size.selected = 0.75
+			
+		apps.weapons.allRooms = false
+		
+		apps.weapons.event = "UserPanel.App.WeaponApp"		
+		
+		
+appsIDs = 
+{
+	[1] = apps.account,
+	[2] = apps.call,
+	[3] = apps.gps,
+	[4] = apps.money,
+	[5] = apps.music,
+	[6] = apps.weapons,
+	[7] = apps.messages
+}
