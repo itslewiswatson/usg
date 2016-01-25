81a8bd9e1b2643014a1abc054928b7b8006a423b
Carl Rizk <carlrizk88@gmail.com>
2016-01-25 16:30:12 +0200

    no message

--- [developers]/[carl]/USGCarlMisc/client/helpGUI.lua
+++ [developers]/[carl]/USGCarlMisc/client/helpGUI.lua
@@ -2,7 +2,7 @@ local helpGUI = {}
 
 addEventHandler("onClientResourceStart", resourceRoot,
     function()
-        helpGUI.window = guiCreateWindow(0.83, 0.80, 0.17, 0.20, "", true)
+        helpGUI.window = guiCreateWindow(0.80, 0.84, 0.20, 0.16, "", true)
         guiWindowSetSizable(helpGUI.window, false)
 
         helpGUI.label = guiCreateLabel(0.04, 0.16, 0.95, 0.79, "", true, helpGUI.window)
