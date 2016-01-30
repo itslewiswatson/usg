5aa79544354dfc65480f49755d17d294cc83a192
c0deninja <tim_developer@hotmail.com>
2016-01-30 16:53:36 +0100

    no message

--- [developers]/[metall]/USGdelivery/deliveryClient.lua
+++ [developers]/[metall]/USGdelivery/deliveryClient.lua
@@ -2,6 +2,10 @@
 
 function testObjects()
 	local px, py, pz = getElementPosition(localPlayer)
-	createObject(1271, px, py, pz)
+	
+	outputChatBox("Spawned a crate", 0, 255, 0)
+	obj = createObject(1271, px, py, pz)
+	marker = createMarker(px, py, pz, "arrow", 1, 0, 255, 0, 200)
+	attachElements(marker, obj)
 end
 addCommandHandler("testobj", testObjects)
