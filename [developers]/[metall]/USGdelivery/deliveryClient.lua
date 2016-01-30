345d0dae2b8df2ce2f93319c2ddba2d78987ddf5
c0deninja <tim_developer@hotmail.com>
2016-01-30 16:48:16 +0100

    no message

---
+++ [developers]/[metall]/USGdelivery/deliveryClient.lua
@@ -0,0 +1,7 @@
+-- Object ID: 1271 & 3798
+
+function testObjects(plr)
+	local px, py, pz = getElementPosition(plr)
+	testObj = createObject(1271, x, y, z)
+end
+addCommandHandler("testObj", testObjects)
